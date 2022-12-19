d5b9457f5a515083869192c7eeb0d710de3c0eef
21wh1a1214 <112647000+21wh1a1214@users.noreply.github.com>
2022-12-19 15:49:51 +0530

    Create vim sample3.cpp

---
+++ vim sample3.cpp
@@ -0,0 +1,22 @@
+//increment and decrement
+#include<iostream>
+using namespace std;
+int main()
+{
+        int a=15,b=20,res_a1,res_b1,res_a2,res_b2;
+        res_a1=++a;
+        res_a2=a++;
+        res_b1=--a;
+        res_b2=a--;
+
+        cout << " pre increment of a = " << res_a1 << endl;
+        cout << " post increment of a = " << res_a2 << endl;
+        cout << " pre decrement of b = " << res_b1 << endl;
+        cout << " post decrement of b = " << res_b2 << endl;
+        return 0;
+}
+
+~                                                                     
+~                                                                     
+~                                                                     
+~ 
